67c8fa61a588ee0a0bea23e173b686e4616b44d0
Ioannis Kokkinis <info@upg.gr>
2017-03-07 22:04:27 +0200

    Updated navbar.php.

--- navbar.php
+++ navbar.php
@@ -11,7 +11,7 @@
 
             <!-- LOGO -->
             <div class="topbar-left">
-                <a href="index.html" class="logo"><span>Greek<span>TV</span></span><i class="zmdi zmdi-layers"></i></a>
+                <a href="index.php" class="logo"><span>Greek<span>TV</span></span><i class="zmdi zmdi-layers"></i></a>
             </div>
 
             <!-- Button mobile view to collapse sidebar menu -->
